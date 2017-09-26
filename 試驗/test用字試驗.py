d46969dd731955c3b8b5b2b59a3a39d0b0ee7aab
薛丞宏 <ihcaoe@gmail.com>
2017-09-26 13:56:32 +0800

    加試驗

--- 試驗/test用字試驗.py
+++ 試驗/test用字試驗.py
@@ -8,23 +8,17 @@ class 用字試驗(TestCase):
     def test_字物件(self):
         self.assertTrue(用字表.有這个字無(字('來', 'lai5')))
 
-    def test_分詞(self):
-        self.assertTrue(用字表.有這个字無('來｜lai5'))
-
-    def test_漢字臺羅分開(self):
-        self.assertTrue(用字表.有這个字無('來', 'lai5'))
+    def test_輕聲有(self):
+        self.assertTrue(用字表.有這个字無(字('來', '0lai5')))
 
     def test_無輕聲(self):
-        self.assertTrue(用字表.有這个字無('來', 'lai5'))
-
-    def test_輕聲有(self):
-        self.assertTrue(用字表.有這个字無('來', 'lai5'))
+        self.assertFalse(用字表.有這个字無(字('巧', '0khiau2')))
 
     def test_干焦輕聲(self):
-        self.assertTrue(用字表.有這个字無('來', 'lai5'))
+        self.assertTrue(用字表.有這个字無(字('矣', '0ah4')))
 
     def test_無實調(self):
-        self.assertTrue(用字表.有這个字無('來', 'lai5'))
+        self.assertTrue(用字表.有這个字無(字('矣', 'ah4')))
 
     def test_羅馬字佮羅馬字(self):
-        self.fail()
+        self.assertFalse(用字表.有這个字無(字('lai5', 'lai5')))
