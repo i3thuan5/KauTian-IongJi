44c9df4c93d866e661842d6fe94608f2e5b5bece
a8568730 <a8568730@hotmail.com>
2019-01-30 12:37:12 +0800

    * 匯入語音方言差表。加test用字語音方言差試驗。

--- 用字/匯入資料.py
+++ 用字/匯入資料.py
@@ -7,6 +7,7 @@ from urllib.request import urlopen
 
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 用字.詞彙方音差函式 import 提出一方音的講法
+from 用字.詞彙方音差函式 import 揣括號
 
 
 class 教典字物件:
@@ -16,16 +17,18 @@ class 教典字物件:
     又音網址 = github + '%E5%8F%88%E9%9F%B3.csv'
     例句網址 = github + '%E4%BE%8B%E5%8F%A5.csv'
     詞彙方音差網址 = github + '%E8%A9%9E%E5%BD%99%E6%96%B9%E8%A8%80%E5%B7%AE.csv'
-
+    語音方言差網址 = github + '%E8%AA%9E%E9%9F%B3%E6%96%B9%E8%A8%80%E5%B7%AE.csv'
+    
     #
     # 撈出教典的字
     #
     @classmethod
     def 全部資料(cls):
-        yield from cls.詞目總檔()
-        yield from cls.又見音表()
-        yield from cls.例句()
-        yield from cls.詞彙方音差()
+#         yield from cls.詞目總檔()
+#         yield from cls.又見音表()
+#         yield from cls.例句()
+#         yield from cls.詞彙方音差()
+        yield from cls.語音方言差()
 
     #
     # 主編碼,屬性,詞目,音讀,文白屬性,部首
@@ -100,7 +103,26 @@ class 教典字物件:
                         講法陣列 = 提出一方音的講法(val)
                         for 一講法 in 講法陣列:
                             yield from 擲出字物件(一講法[0], 一講法[1])
-
+    #
+    # 字的方言表
+    #
+    @classmethod
+    def 語音方言差(cls):
+        with urlopen(cls.語音方言差網址) as 檔:
+            with io.StringIO(檔.read().decode()) as 表:
+                for row in DictReader(表):
+                    漢字 = 揣括號.sub('', row['字目'])
+                    for key in ['鹿港','三峽','臺北','宜蘭','臺南','高雄',
+                                '金門','馬公','新竹','臺中',]:
+                        # 該腔口無收著
+                        if row[key] == 'x':
+                            continue
+                        # 提出一个腔口的無仝講法
+                        音 = 揣括號.sub('', row[key]).replace(' ', '')
+                        音陣列 = 音.split(';')
+                        for 一音 in 音陣列:
+                            yield from 擲出字物件(漢字, 一音)
+                            
 
 def 擲出字物件(句漢, 句羅):
     try:
