38ab9ddfb193f1aa5ef44b3d0bd0a47cdbe3d5e0
a8568730 <a8568730@hotmail.com>
2019-02-18 12:27:49 +0800

    + 用字表/test設定試驗.py。當咧試django.setting開關教典佮標點

---
+++ 試驗/用字表/test設定試驗.py
@@ -0,0 +1,27 @@
+from django.test.testcases import TestCase
+from 臺灣言語工具.基本物件.字 import 字
+from 用字.models import 用字表
+
+
+class 設定試驗(TestCase):
+    def test_預設開教典(self):
+        self.assertTrue(用字表.有這个字無(字('媠', 'suí')))
+    
+    def test_預設開標點(self):
+        self.assertTrue(用字表.有這个字無(字('！', '!')))
+
+    def test_共教典開開(self):
+        with self.settings(敢開教典=True):
+            self.assertTrue(用字表.有這个字無(字('媠', 'suí')))
+
+    def test_共教典關掉(self):
+        with self.settings(敢開教典=False):
+            self.assertFalse(用字表.有這个字無(字('媠', 'suí')))
+
+    def test_共標點開開(self):
+        with self.settings(敢開標點=True):
+            self.assertTrue(用字表.有這个字無(字('！', '!')))
+
+    def test_共標點關掉(self):
+        with self.settings(敢開標點=False):
+            self.assertFalse(用字表.有這个字無(字('！', '!')))
