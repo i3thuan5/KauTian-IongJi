c5a8f9b04d7ce28ea4a656fc8b712e8429e57fde
a8568730 <a8568730@hotmail.com>
2019-02-18 15:47:43 +0800

    * 用字表，先看教典、標點才看用字表

--- 用字/models.py
+++ 用字/models.py
@@ -1,6 +1,9 @@
 from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 from django.db import models
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+from django.conf import settings
+from 用字 import 標點
+from 用字 import 教典
 
 
 class 用字表(models.Model):
@@ -12,8 +15,19 @@ class 用字表(models.Model):
     def 有這个字無(cls, 字物件):
         字臺羅物件 = 字物件.轉音(臺灣閩南語羅馬字拼音)
         字臺羅物件.音 = 字臺羅物件.音.lstrip('0')
-        字分詞 = 字臺羅物件.看分詞()
-        return cls.objects.filter(分詞=字分詞).exists()
+        有無 = False
+        # 先揣教典、標點json
+        敢開教典 = getattr(settings, '敢開教典', True)
+        敢開標點 = getattr(settings, '敢開標點', True)
+        if 敢開教典:
+            有無 = 有無 or 教典.有這个字無(字臺羅物件)
+        if 敢開標點:
+            有無 = 有無 or 標點.有這个字無(字臺羅物件)
+        # 揣用字表
+        if 有無 is False:
+            字分詞 = 字臺羅物件.看分詞()
+            有無 = 有無 or cls.objects.filter(分詞=字分詞).exists()
+        return 有無
 
     def save(self, *args, **kwargs):
         # 提掉舊的輕聲規範
--- 試驗/用字表/test設定試驗.py
+++ 試驗/用字表/test設定試驗.py
@@ -1,27 +1,29 @@
-from django.test.testcases import TestCase
+from django.test import TestCase
 from 臺灣言語工具.基本物件.字 import 字
 from 用字.models import 用字表
+from django.test.utils import override_settings
+from django.conf import settings
 
 
 class 設定試驗(TestCase):
     def test_預設開教典(self):
         self.assertTrue(用字表.有這个字無(字('媠', 'suí')))
-    
+
     def test_預設開標點(self):
         self.assertTrue(用字表.有這个字無(字('！', '!')))
 
+    @override_settings(敢開教典=True)
     def test_共教典開開(self):
-        with self.settings(敢開教典=True):
-            self.assertTrue(用字表.有這个字無(字('媠', 'suí')))
+        self.assertTrue(用字表.有這个字無(字('媠', 'suí')))
 
+    @override_settings(敢開教典=False)
     def test_共教典關掉(self):
-        with self.settings(敢開教典=False):
-            self.assertFalse(用字表.有這个字無(字('媠', 'suí')))
+        self.assertFalse(用字表.有這个字無(字('媠', 'suí')))
 
+    @override_settings(敢開標點=True)
     def test_共標點開開(self):
-        with self.settings(敢開標點=True):
-            self.assertTrue(用字表.有這个字無(字('！', '!')))
+        self.assertTrue(用字表.有這个字無(字('！', '!')))
 
+    @override_settings(敢開標點=False)
     def test_共標點關掉(self):
-        with self.settings(敢開標點=False):
-            self.assertFalse(用字表.有這个字無(字('！', '!')))
+        self.assertFalse(用字表.有這个字無(字('！', '!')))
