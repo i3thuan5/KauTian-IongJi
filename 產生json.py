e8d06c741e89d4e24bf094c6de317467ab3def33
Fafofafoy <ihcaoe@gmail.com>
2019-06-20 17:13:57 +0800

    San-sing json kap tso-hue.

--- 產生json.py
+++ 產生json.py
@@ -2,11 +2,33 @@
 import json
 from os.path import join, abspath, dirname
 from 教典.匯入甘字典 import 甘字典字物件
+from 教典.匯入資料 import 教典字物件
 
 
-def 產生json():
-    檔名 = join(dirname(abspath(__file__)), '用字','甘字典.json')
-    
+def main():
+    產生教典json()
+    產生甘字典json()
+
+
+def 產生教典json():
+    教典檔名 = join(dirname(abspath(__file__)), '用字', '教典.json')
+
+    全部用字 = set()
+    for 字物件 in 教典字物件.全部資料():
+        全部用字.add(
+            字物件.看分詞()
+        )
+    with open(教典檔名, 'w') as 檔案:
+        json.dump(
+            sorted(全部用字), 檔案,
+            indent=2,
+            ensure_ascii=False
+        )
+
+
+def 產生甘字典json():
+    檔名 = join(dirname(abspath(__file__)), '用字', '甘字典.json')
+
     全部用字 = set()
     for 字物件 in 甘字典字物件().詞目():
         全部用字.add(
@@ -21,4 +43,4 @@ def 產生json():
 
 
 if __name__ == '__main__':
-    產生json()
+    main()
