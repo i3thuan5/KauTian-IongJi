5671e4211758256e02eb3273e541679055333f35
Fafofafoy <ihcaoe@gmail.com>
2019-06-20 17:54:55 +0800

    Tsing-li iong-ji-pio.

--- 用字/models.py
+++ 用字/models.py
@@ -1,37 +1,37 @@
 from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
+from django.conf import settings
 from django.db import models
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-from django.conf import settings
 from 用字 import 標點
 from 用字 import 教典
 from 用字 import 甘字典
+
+
+def _theh用字ê範圍():
+    範圍 = set()
+    if getattr(settings, '敢開教典', True):
+        範圍 |= 教典.全部分詞()
+    if getattr(settings, '敢開標點', True):
+        範圍 |= 標點.全部分詞()
+    if getattr(settings, '敢開甘字典', False):
+        範圍 |= 甘字典.全部分詞()
+    return 範圍
 
 
 class 用字表(models.Model):
     漢字 = models.CharField(max_length=5)
     羅馬字 = models.CharField(max_length=15)
     分詞 = models.CharField(max_length=20)
+    _用字ê範圍 = _theh用字ê範圍()
 
     @classmethod
     def 有這个字無(cls, 字物件):
         字臺羅物件 = 字物件.轉音(臺灣閩南語羅馬字拼音)
         字臺羅物件.音 = 字臺羅物件.音.lstrip('0')
-        有無 = False
-        # 先揣教典、標點json
-        敢開教典 = getattr(settings, '敢開教典', True)
-        敢開標點 = getattr(settings, '敢開標點', True)
-        敢開甘字典 = getattr(settings, '敢開甘字典', False)
-        if 敢開教典:
-            有無 = 有無 or 教典.有這个字無(字臺羅物件)
-        if 敢開標點:
-            有無 = 有無 or 標點.有這个字無(字臺羅物件)
-        if 敢開甘字典:
-            有無 = 有無 or 甘字典.有這个字無(字臺羅物件)
-        # 揣用字表
-        if 有無 is False:
-            字分詞 = 字臺羅物件.看分詞()
-            有無 = 有無 or cls.objects.filter(分詞=字分詞).exists()
-        return 有無
+        字分詞 = 字臺羅物件.看分詞()
+        if 字分詞 in cls._用字ê範圍:
+            return True
+        return cls.objects.filter(分詞=字分詞).exists()
 
     def save(self, *args, **kwargs):
         # 提掉舊的輕聲規範
--- 試驗/用字表/test設定試驗.py
+++ 試驗/用字表/test設定試驗.py
@@ -1,10 +1,15 @@
 from django.test import TestCase
+from django.test.utils import override_settings
 from 臺灣言語工具.基本物件.字 import 字
 from 用字.models import 用字表
-from django.test.utils import override_settings
+from 用字.models import _theh用字ê範圍
 
 
 class 設定試驗(TestCase):
+    def tearDown(self):
+        '愛改轉來，別人才袂出問題'
+        self.重讀用字設定()
+
     def test_預設開教典(self):
         self.assertTrue(用字表.有這个字無(字('媠', 'suí')))
 
@@ -16,24 +21,33 @@ class 設定試驗(TestCase):
 
     @override_settings(敢開教典=True)
     def test_共教典開開(self):
+        self.重讀用字設定()
         self.assertTrue(用字表.有這个字無(字('媠', 'suí')))
 
     @override_settings(敢開教典=False)
     def test_共教典關掉(self):
+        self.重讀用字設定()
         self.assertFalse(用字表.有這个字無(字('媠', 'suí')))
 
     @override_settings(敢開標點=True)
     def test_共標點開開(self):
+        self.重讀用字設定()
         self.assertTrue(用字表.有這个字無(字('！', '!')))
 
     @override_settings(敢開標點=False)
     def test_共標點關掉(self):
+        self.重讀用字設定()
         self.assertFalse(用字表.有這个字無(字('！', '!')))
 
     @override_settings(敢開甘字典=True)
     def test_共甘字典開開(self):
+        self.重讀用字設定()
         self.assertTrue(用字表.有這个字無(字('靄', 'Ai2')))
 
     @override_settings(敢開甘字典=False)
     def test_共甘字典關掉(self):
+        self.重讀用字設定()
         self.assertFalse(用字表.有這个字無(字('靄', 'Ai2')))
+
+    def 重讀用字設定(self):
+        用字表._用字ê範圍 = _theh用字ê範圍()
