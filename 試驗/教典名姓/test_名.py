9c52f027d8b8e78739b8443e1a8d967f6163eb18
Fafofafoy <ihcaoe@gmail.com>
2020-05-12 15:10:12 +0800

    Ka tshi-giam

---
+++ 試驗/教典名姓/test_名.py
@@ -0,0 +1,11 @@
+from django.test.testcases import TestCase
+
+
+class Mia(TestCase):
+    'https://docs.google.com/spreadsheets/u/1/d/1yNKBf8NcYj00iH6zVETMB9LwNZf0HCg0fekp8zlNrRY/export?format=csv&id=1yNKBf8NcYj00iH6zVETMB9LwNZf0HCg0fekp8zlNrRY&gid=1608156122'
+
+    def test_erm(self):
+        self.assertTrue(教典名姓.有這个字無(字('鍼', 'tserm')))
+
+    def test_ionn(self):
+        self.assertTrue(教典名姓.有這个字無(字('薔', 'tshiônn')))
