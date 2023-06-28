5fac1ee3e2c75e1bc2c681595f4c2903ed1b0cf5
Tira <hong@ithuan.tw>
2023-06-28 10:55:00 +0800

    整理試驗。

--- 試驗/教典名姓/test_名.py
+++ 試驗/教典名姓/test_名.py
@@ -1,23 +1,23 @@
 from unittest.case import TestCase
 from 臺灣言語工具.基本物件.字 import 字
-from 用字 import 教典名姓
+from 用字 import 教典
 
 
 class Mia(TestCase):
 
     def test_erm(self):
-        self.assertTrue(教典名姓.有這个字無(字('鍼', 'tserm')))
+        self.assertTrue(教典.有這个字無(字('鍼', 'tserm')))
 
     def test_ionn(self):
-        self.assertTrue(教典名姓.有這个字無(字('薔', 'tshiônn')))
+        self.assertTrue(教典.有這个字無(字('薔', 'tshiônn')))
 
     def test_異體字mài_較袂攪擾(self):
-        self.assertFalse(教典名姓.有這个字無(字('兔', 'thoo2')))
+        self.assertFalse(教典.有這个字無(字('兔', 'thoo2')))
 
     def test_訓用mài(self):
         '教典本文收才準算'
-        self.assertFalse(教典名姓.有這个字無(字('呆', 'phái')))
+        self.assertFalse(教典.有這个字無(字('呆', 'phái')))
 
     def test_俗用mài(self):
         '教典本文收才準算'
-        self.assertFalse(教典名姓.有這个字無(字('殯', 'pin')))
+        self.assertFalse(教典.有這个字無(字('殯', 'pin')))
