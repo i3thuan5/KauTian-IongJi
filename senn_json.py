ab4820883919f2793acbfba8ab92c39aee0acfe6
Tira <hong@ithuan.tw>
2023-06-28 10:25:37 +0800

    用新教典ods。

--- senn_json.py
+++ senn_json.py
@@ -1,48 +1,27 @@
 # -*- coding: utf-8 -*-
 import json
 from os.path import join, abspath, dirname
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from 教典.匯入甘字典 import 甘字典字物件
 from 教典.匯入資料 import 教典字物件
-from 教典.匯入教典名姓 import 教典名姓字物件
 
 
 def main():
     產生教典json()
-    產生教典名姓json()
     產生甘字典json()
 
 
 def 產生教典json():
     教典檔名 = join(dirname(abspath(__file__)), '用字', '教典.json')
 
-    全部用字 = set()
-    for 字物件 in 教典字物件.全部資料():
-        字物件.型 = 字物件.型.lstrip('-')
-        字物件.音 = 字物件.音.lstrip('-')
-        字物件.輕聲標記 = False
-        全部用字.add(
+    全部用字 = []
+
+    for han, lo in set(教典字物件().全部資料()):
+        字物件 = 拆文分析器.建立字物件(han.lstrip('-'), lo.lstrip('-'))
+        全部用字.append(
             字物件.看分詞()
         )
     with open(教典檔名, 'w') as 檔案:
-        json.dump(
-            sorted(全部用字), 檔案,
-            indent=2,
-            ensure_ascii=False
-        )
-
-
-def 產生教典名姓json():
-    教典名姓檔名 = join(dirname(abspath(__file__)), '用字', '教典名姓.json')
-
-    全部用字 = set()
-    for 字物件 in 教典名姓字物件.全部資料():
-        字物件.型 = 字物件.型.lstrip('-')
-        字物件.音 = 字物件.音.lstrip('-')
-        字物件.輕聲標記 = False
-        全部用字.add(
-            字物件.看分詞()
-        )
-    with open(教典名姓檔名, 'w') as 檔案:
         json.dump(
             sorted(全部用字), 檔案,
             indent=2,
--- 教典/匯入資料.py
+++ 教典/匯入資料.py
@@ -1,152 +1,43 @@
 # -*- coding: utf-8 -*-
-
-from csv import DictReader
 import io
 from urllib.request import urlopen
-
-
-from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
-from 臺灣言語工具.解析整理.解析錯誤 import 解析錯誤
-from 教典.詞彙方言差函式 import 提出一方言的講法
-from 教典.詞彙方言差函式 import 揣括號
+from pyexcel_ods3 import get_data
+from kesi import Ku, TuiBeTse
+from kesi.butkian.kongiong import 標點符號
 
 
 class 教典字物件:
-    github = 'https://github.com/g0v/moedict-data-twblg/raw/master/uni/'
-    詞目總檔網址 = github + '%E8%A9%9E%E7%9B%AE%E7%B8%BD%E6%AA%94.csv'
-    詞目總檔屬性網址 = github + '%E8%A9%9E%E7%9B%AE%E7%B8%BD%E6%AA%94.%E8%A9%9E%E7%9B%AE%E5%B1%AC%E6%80%A7%E5%B0%8D%E7%85%A7.csv'
-    又音網址 = github + '%E5%8F%88%E9%9F%B3.csv'
-    例句網址 = github + '%E4%BE%8B%E5%8F%A5.csv'
-    詞彙方言差網址 = github + '%E8%A9%9E%E5%BD%99%E6%96%B9%E8%A8%80%E5%B7%AE.csv'
-    語音方言差網址 = github + '%E8%AA%9E%E9%9F%B3%E6%96%B9%E8%A8%80%E5%B7%AE.csv'
+    教典ods網址 = 'https://sutian.moe.edu.tw/media/kautian.ods'
 
-    #
-    # 撈出教典的字
-    #
-    @classmethod
-    def 全部資料(cls):
-        yield from cls.詞目總檔()
-        yield from cls.又見音表()
-        yield from cls.詞彙方言差()
-        yield from cls.語音方言差()
-        '內政部 宗教百景'
-        yield from 擲出字物件('𰹬', 'tsng7')
+    def 全部資料(self):
+        yield from self.新教典ods()
+        '教育部《咱來學臺灣閩南語：讀文章蓋趣味01》、內政部 宗教百景'
+        yield '𰹬', 'tsn̄g'
 
-    #
-    # 主編碼,屬性,詞目,音讀,文白屬性,部首
-    # 6,1,一月日,tsi̍t gue̍h-ji̍t/tsi̍t ge̍h-li̍t,0,
-    #
-    @classmethod
-    def 詞目總檔(cls):
-        print('匯入詞目總檔...')
-        會使的屬性 = set()
-        with urlopen(cls.詞目總檔屬性網址) as 檔:
+    def 新教典ods(self):
+        with urlopen(self.教典ods網址) as 檔:
             with io.StringIO(檔.read().decode()) as 資料:
-                for row in DictReader(資料):
-                    if '地名' not in row['屬性'].strip():
-                        會使的屬性.add(row['編號'].strip())
-        with urlopen(cls.詞目總檔網址) as 檔:
-            with io.StringIO(檔.read().decode()) as 資料:
-                for row in DictReader(資料):
-                    音讀 = row['音讀'].strip()
-                    if 音讀 == '' or row['屬性'].strip() not in 會使的屬性:
-                        continue
-                    漢字 = row['詞目'].strip()
-                    for 一音 in 音讀.split('/'):
-                        臺羅 = 一音.strip()
-                        yield from 擲出字物件(漢字, 臺羅)
+                kiatko = get_data(資料)
 
-    #
-    # 該詞目在漳泉腔音讀以外的又見音。
-    # 序號 主編碼 又音 又音類型(1.又唸作 2.俗唸作 3.合音唸作)
-    #   3   72    tsa̍p-jī-tsí-tn̂g/tsa̍p-lī-tsí-tn̂g    1
-    #
-    @classmethod
-    def 又見音表(cls):
-        print('匯入又見音表...')
-        資料 = {}
-        with urlopen(cls.詞目總檔網址) as 檔:
-            with io.StringIO(檔.read().decode()) as 字串資料:
-                for row in DictReader(字串資料):
-                    主編碼 = row['主編碼'].strip()
-                    漢字 = row['詞目'].strip()
-                    資料[主編碼] = 漢字
+        for mia, ji in self._tshue_ji(kiatko):
+            phe = ji.hanlo.lower(), ji.lomaji.lower()
+            if ji.hanlo not in 標點符號:
+                return phe
 
-        with urlopen(cls.又音網址) as 檔:
-            with io.StringIO(檔.read().decode()) as 字串資料:
-                for row in DictReader(字串資料):
-                    if row['又音類型(1.又唸作 2.俗唸作 3.合音唸作)'] == '3':
-                        continue
-                    主編碼 = row['主編碼'].strip()
-                    漢字 = 資料[主編碼]
-                    for 一音 in row['又音'].split('/'):
-                        臺羅 = 一音.strip()
-                        yield from 擲出字物件(漢字, 臺羅)
-
-    #
-    # 從例句檔撈字。
-    # 因為詞目總檔不見得有包括例句的字。
-    # （停用）
-    #
-    @classmethod
-    def 例句(cls):
-        print('匯入例句表...')
-        with urlopen(cls.例句網址) as 檔:
-            with io.StringIO(檔.read().decode()) as 資料:
-                for row in DictReader(資料):
-                    音讀 = row['例句標音'].strip()
-                    漢字 = row['例句'].strip()
-                    yield from 擲出字物件(漢字, 音讀)
-
-    @classmethod
-    def 詞彙方言差(cls):
-        print('匯入詞彙方言差...')
-        with urlopen(cls.詞彙方言差網址) as 檔:
-            with io.StringIO(檔.read().decode()) as 表:
-                for row in DictReader(表):
-                    for key, val in sorted(row.items()):
-                        if key in ['序號', '方言差編碼', '詞目']:
-                            continue
-                        講法陣列 = 提出一方言的講法(val)
-                        for 一講法 in 講法陣列:
-                            yield from 擲出字物件(一講法[0], 一講法[1])
-    #
-    # 字的方言表
-    #
-
-    @classmethod
-    def 語音方言差(cls):
-        print('匯入語音方言差...')
-        with urlopen(cls.語音方言差網址) as 檔:
-            with io.StringIO(檔.read().decode()) as 表:
-                for row in DictReader(表):
-                    漢字 = 揣括號.sub('', row['字目'])
-                    for key in ['鹿港', '三峽', '臺北', '宜蘭', '臺南', '高雄',
-                                '金門', '馬公', '新竹', '臺中', ]:
-                        # 該腔口無收著
-                        if row[key] == 'x':
-                            continue
-                        # 提出一个腔口的無仝講法
-                        音 = 揣括號.sub('', row[key]).replace(' ', '')
-                        音陣列 = 音.split(';')
-                        for 一音 in 音陣列:
-                            yield from 擲出字物件(漢字, 一音)
-
-
-def 擲出字物件(句漢, 句羅):
-    音讀正確 = True
-    try:
-        for 字物件 in (
-            拆文分析器
-            .對齊句物件(句漢, 句羅)
-            .篩出字物件()
-        ):
-            if 字物件.音標敢著(臺灣閩南語羅馬字拼音):
-                yield 字物件.轉音(臺灣閩南語羅馬字拼音)
-            else:
-                音讀正確 = False
-    except 解析錯誤 as e:
-        print('擲出字物件:', str(e))
-    if 音讀正確 is False:
-        print('音標重耽：', 句漢, 句羅)
+    def _tshue_ji(self, kiatko):
+        for mia, pio in kiatko.items():
+            piaute = pio[0]
+            try:
+                hanjiui = piaute.index('漢字')
+                lomajiui = piaute.index('羅馬字')
+            except ValueError:
+                continue
+            for tsua in pio[1:]:
+                hanji = tsua[hanjiui]
+                lomajitin = tsua[lomajiui]
+                for lomaji in lomajitin.split('/'):
+                    try:
+                        for ji in Ku(hanji, lomaji).thianji():
+                            yield mia, ji
+                    except TuiBeTse:
+                        pass
