5f202a101e37d1232eddb018494eb3d34868af86
Tira <hong@ithuan.tw>
2023-06-28 10:35:19 +0800

    教典名姓kap入去教典，甘字典無tih用，嘛bua̋i huānn--ah。

--- 用字/公家變數.py
+++ 用字/公家變數.py
@@ -2,5 +2,3 @@
 from os.path import join, abspath, dirname
 
 教典檔名 = join(dirname(abspath(__file__)), '教典.json')
-甘字典檔名 = join(dirname(abspath(__file__)), '甘字典.json')
-教典名姓檔名 = join(dirname(abspath(__file__)), '教典名姓.json')
