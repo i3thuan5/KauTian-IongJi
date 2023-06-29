c34070c28670bbb4edd3ef9eb40f252e6555f9e0
Tira <hong@ithuan.tw>
2023-06-29 11:59:37 +0800

    tsinn新詞khioh出來。

--- 試驗/用字表/test用字表試驗.py
+++ 試驗/用字表/test用字表試驗.py
@@ -5,7 +5,18 @@ from django.core.exceptions import ValidationError
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 
 
-class 用字表試驗(TestCase):
+class 用字表加詞:
+    def 用字表create(self, 漢字, 羅馬字):
+        # client = Client()
+        # client.login()
+        # huein = client.post()
+        # self.assertEqual(huein.status_code, 302)
+        ji = 用字表(漢字=漢字, 羅馬字=羅馬字)
+        ji.full_clean()
+        ji.save()
+
+
+class 用字表試驗(TestCase, 用字表加詞):
 
     def test_無加字(self):
         漢 = '媠'
@@ -15,19 +26,19 @@ class 用字表試驗(TestCase):
     def test_加一字(self):
         漢 = '媠'
         羅 = 'khiáu'
-        用字表create(漢字=漢, 羅馬字=羅)
+        self.用字表create(漢字=漢, 羅馬字=羅)
         self.assertTrue(用字表.有這个字無(拆文分析器.對齊字物件(漢, 羅)))
 
     def test_數字調(self):
         漢 = '媠'
         羅 = 'khiau2'
-        用字表create(漢字=漢, 羅馬字=羅)
+        self.用字表create(漢字=漢, 羅馬字=羅)
         self.assertTrue(用字表.有這个字無(拆文分析器.對齊字物件(漢, 羅)))
 
     def test_大寫(self):
         漢 = '媠'
         羅 = 'Khiáu'
-        用字表create(漢字=漢, 羅馬字=羅)
+        self.用字表create(漢字=漢, 羅馬字=羅)
         self.assertTrue(用字表.有這个字無(拆文分析器.對齊字物件(漢, 羅)))
 
     def test_一定àithinn字bēsái詞(self):
@@ -39,41 +50,35 @@ class 用字表試驗(TestCase):
     def test_加符號(self):
         漢 = '~'
         羅 = '―'
-        用字表create(漢字=漢, 羅馬字=羅)
+        self.用字表create(漢字=漢, 羅馬字=羅)
         self.assertTrue(用字表.有這个字無(拆文分析器.對齊字物件(漢, 羅)))
 
     def test_輕聲符(self):
-        用字表create(漢字='媠', 羅馬字='--khiáu')
+        self.用字表create(漢字='媠', 羅馬字='--khiáu')
         self.assertTrue(用字表.有這个字無(
             拆文分析器.對齊字物件('媠', '--khiáu')))
 
     def test_輕聲符2(self):
-        用字表create(漢字='媠', 羅馬字='--khiáu')
+        self.用字表create(漢字='媠', 羅馬字='--khiáu')
         self.assertTrue(用字表.有這个字無(
             拆文分析器.對齊字物件('媠', 'khiáu')))
 
     def test_輕聲符4(self):
-        用字表create(漢字='媠', 羅馬字='khiáu')
+        self.用字表create(漢字='媠', 羅馬字='khiáu')
         self.assertTrue(用字表.有這个字無(
             拆文分析器.對齊字物件('媠', '--khiáu')))
 
     def test_0輕聲符(self):
-        用字表create(漢字='媠', 羅馬字='0khiáu')
+        self.用字表create(漢字='媠', 羅馬字='0khiáu')
         self.assertTrue(用字表.有這个字無(
             拆文分析器.對齊字物件('媠', '0khiáu')))
 
     def test_0輕聲符2(self):
-        用字表create(漢字='媠', 羅馬字='0khiáu')
+        self.用字表create(漢字='媠', 羅馬字='0khiáu')
         self.assertTrue(用字表.有這个字無(
             拆文分析器.對齊字物件('媠', 'khiáu')))
 
     def test_0輕聲符4(self):
-        用字表create(漢字='媠', 羅馬字='khiáu')
+        self.用字表create(漢字='媠', 羅馬字='khiáu')
         self.assertTrue(用字表.有這个字無(
             拆文分析器.對齊字物件('媠', '0khiáu')))
-
-
-def 用字表create(漢字, 羅馬字):
-    ji = 用字表(漢字=漢字, 羅馬字=羅馬字)
-    ji.full_clean()
-    ji.save()
--- 試驗/用字表/test這馬表試驗.py
+++ 試驗/用字表/test這馬表試驗.py
@@ -1,20 +1,20 @@
 from 用字.models import 用字表
 from django.test.testcases import TestCase
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
-from 試驗.用字表.test用字表試驗 import 用字表create
+from 試驗.用字表.test用字表試驗 import 用字表加詞
 
 
-class 這馬表試驗(TestCase):
+class 這馬表試驗(TestCase, 用字表加詞):
 
     def test_有加著字(self):
         漢 = '媠'
         羅 = 'khiáu'
-        用字表create(漢字=漢, 羅馬字=羅)
+        self.用字表create(漢字=漢, 羅馬字=羅)
         self.assertTrue(用字表.這馬().有這个字無(拆文分析器.對齊字物件(漢, 羅)))
 
     def test_提出來了後就bôtshap資料庫有加著字(self):
         漢 = '媠'
         羅 = 'khiáu'
         表 = 用字表.這馬()
-        用字表create(漢字=漢, 羅馬字=羅)
+        self.用字表create(漢字=漢, 羅馬字=羅)
         self.assertFalse(表.有這个字無(拆文分析器.對齊字物件(漢, 羅)))
